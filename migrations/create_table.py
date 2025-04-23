0e56f11f71f2cc72125f571fedeb66030cdccffb
JIMBE <adriyansyahmf0@gmail.com>
2025-04-23 23:33:49 +0700

    Animex

--- migrations/create_table.py
+++ migrations/create_table.py
@@ -5,17 +5,19 @@ from models.animes import AnimesModel
 from models.crawler import CrawlersModel
 from models.settings import SiteSettingsModel
 async def main():
-    async with engine.begin() as conn:
 
-        await conn.run_sync(meta.drop_all)
-        await conn.run_sync(meta.create_all, tables=[
-            AdminModel,
-            CrawlersModel,
-            AnimesModel,
-            SiteSettingsModel,
-        ] )
+    #async with engine.begin() as conn:
 
-    await engine.dispose()
+        #await conn.run_sync(meta.drop_all)
+        #await conn.run_sync(meta.create_all, tables=[
+            #AdminModel,
+            #CrawlersModel,
+            #AnimesModel,
+            #SiteSettingsModel,
+        #] )
+
+    #await engine.dispose()
+    print("Creating tables...")
 
 if __name__ == '__main__':
     asyncio.run(main())

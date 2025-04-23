b8c432b8cb7dc757d244261ba2de037631d303c9
JIMBE <adriyansyahmf0@gmail.com>
2025-04-23 23:39:32 +0700

    Animex

--- migrations/create_admin.py
+++ migrations/create_admin.py
@@ -8,18 +8,18 @@ from helpers.general import generate_uuid_from_username
 import asyncio
 
 async def main():
-    async with engine.begin() as conn:
-        hasher = PasswordHasher(BasicSalt(cfg.password.salt))
-        password_hash = hasher.hash('admin')
-        uuid = generate_uuid_from_username('admin')
-        query =  AdminModel.insert().values(
-            name='admin',
-            password=password_hash,
-            uuid=uuid,
-            api_key=secrets.token_urlsafe(32)
-        )
-
-        return await conn.execute(query)
+    #async with engine.begin() as conn:
+        #hasher = PasswordHasher(BasicSalt(cfg.password.salt))
+        #password_hash = hasher.hash('admin')
+        #uuid = generate_uuid_from_username('admin')
+        #query =  AdminModel.insert().values(
+        #    name='admin',
+        #    password=password_hash,
+        #    uuid=uuid,
+        #    api_key=secrets.token_urlsafe(32)
+        #)
+        print("ok")
+        #return await conn.execute(query)
 
 if __name__ == '__main__':
     asyncio.run(main())
